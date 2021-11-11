31c033e8a9d039a6a8b1e75585112523a712d233
Doubleneck <antti.vuorenmaa@kolumbus.fi>
2021-11-11 23:03:40 +0200

    add more functions to operations.py

--- src/services/operations.py
+++ src/services/operations.py
@@ -5,3 +5,23 @@ class Operations:
 
     def sum_func(number1:str, number2:str):
         return float(number1)+float(number2)
+
+    def sub_func(number1:str, number2:str):
+        return float(number1)-float(number2)
+
+    def mul_func(number1:str, number2:str):
+        return float(number1)*float(number2)
+
+    def div_func(number1:str, number2:str):
+        return float(number1)/float(number2)
+
+    def exp_func(number1:str):
+        return float(number1)*float(number1)
+
+    def sqrt_func(number1:str):
+        return sqrt(float(number1))
+
+    def get_pi():
+        return pi   
+
+
