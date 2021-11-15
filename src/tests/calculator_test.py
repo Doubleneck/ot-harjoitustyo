d39d77bfe4b4cf62ea1eac372b7ec0f3796f622f
Doubleneck <antti.vuorenmaa@kolumbus.fi>
2021-11-15 23:51:37 +0200

    add tests calculator.py

--- src/tests/calculator_test.py
+++ src/tests/calculator_test.py
@@ -3,7 +3,17 @@ from services.calculator import Calculator
 
 class TestCalculator(unittest.TestCase):
     def setUp(self):
-        print("Set up goes here")
+        self.calculator = Calculator()
+        self.calculator.set_operand1(16)
 
     def test_hello_world(self):
         self.assertEqual("Hello world", "Hello world")
+
+    def test_add_positive_integer(self):        
+        self.assertEqual(self.calculator.count_two_operands("+",5), "21.0")    
+
+    def test_sub_positive_integer(self):        
+        self.assertEqual(self.calculator.count_two_operands("-",5), "11.0")  
+
+    def test_sq(self):        
+        self.assertEqual(self.calculator.count_one_operands("sq"), "4.0")      
