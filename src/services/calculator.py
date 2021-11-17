2a5cf4d982e102bf1fe42eb33e38b496a0175183
Doubleneck <antti.vuorenmaa@kolumbus.fi>
2021-11-17 15:13:15 +0200

    clean code calculator.py

--- src/services/calculator.py
+++ src/services/calculator.py
@@ -1,15 +1,15 @@
 from services.operations import Operations
 
-class Calculator:   
+class Calculator:
     def __init__(self):
         self.operand1 = "0"
 
     def get_operand1(self):
-        return str(self.operand1)    
+        return str(self.operand1)
 
     def set_operand1(self,number:str):
-        self.operand1 = number     
-        
+        self.operand1 = number
+
     def count_two_operands(self,operator: str,number2:str):
         operand1 = self.operand1
         operand2 = number2
@@ -20,12 +20,12 @@ class Calculator:
         if operator == "*":
             self.operand1  = Operations.mul_func(operand1,operand2)
         if operator == "/":
-            self.operand1  = Operations.div_func(operand1,operand2) 
-        return self.operand1  
+            self.operand1  = Operations.div_func(operand1,operand2)
+        return self.operand1
 
     def count_one_operands(self,operator: str):
-        if operator == "e":   
+        if operator == "e":
             self.operand1 = Operations.exp_func(self.operand1)
-        if operator == "sq": 
-            self.operand1 = Operations.sqrt_func(self.operand1)    
-        return self.operand1 
+        if operator == "sq":
+            self.operand1 = Operations.sqrt_func(self.operand1)
+        return self.operand1
