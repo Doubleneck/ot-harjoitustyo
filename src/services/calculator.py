bf93b4caa52f43127b709706e64e1fbf17ef75a0
Doubleneck <antti.vuorenmaa@kolumbus.fi>
2021-11-17 22:30:20 +0200

    code clean calculator_test.py, operations.py, calculator.py

--- src/services/calculator.py
+++ src/services/calculator.py
@@ -10,22 +10,20 @@ class Calculator:
     def set_operand1(self,number:str):
         self.operand1 = number
 
-    def count_two_operands(self,operator: str,number2:str):
-        operand1 = self.operand1
-        operand2 = number2
+    def count_two_operands(self, operator:str, operand2:str):
         if operator == "+":
-            self.operand1 = Operations.sum_func(operand1,operand2)
+            self.operand1 = Operations.sum_func(self,self.operand1,operand2)
         if operator == "-":
-            self.operand1  = Operations.sub_func(operand1,operand2)
+            self.operand1  = Operations.sub_func(self,self.operand1,operand2)
         if operator == "*":
-            self.operand1  = Operations.mul_func(operand1,operand2)
+            self.operand1  = Operations.mul_func(self,self.operand1,operand2)
         if operator == "/":
-            self.operand1  = Operations.div_func(operand1,operand2)
+            self.operand1  = Operations.div_func(self,self.operand1,operand2)
         return self.operand1
 
     def count_one_operands(self,operator: str):
         if operator == "e":
-            self.operand1 = Operations.exp_func(self.operand1)
+            self.operand1 = Operations.exp_func(self,self.operand1)
         if operator == "sq":
-            self.operand1 = Operations.sqrt_func(self.operand1)
+            self.operand1 = Operations.sqrt_func(self,self.operand1)
         return self.operand1
--- src/services/operations.py
+++ src/services/operations.py
@@ -1,30 +1,28 @@
 from math import sqrt
-from math import pi
+#from math import pi
 
 class Operations:
 
-    def sum_func(number1:str, number2:str):
+    def sum_func(self, number1:str, number2:str):
         return str(float(number1)+float(number2))
 
-    def sub_func(number1:str, number2:str):
+    def sub_func(self, number1:str, number2:str):
         return str(float(number1) - float(number2))
 
-    def mul_func(number1:str, number2:str):
+    def mul_func(self, number1:str, number2:str):
         return str(float(number1) * float(number2))
 
-    def div_func(number1:str, number2:str):
+    def div_func(self, number1:str, number2:str):
         try:
             return str(float(number1) / float(number2))
-        except:
-             return "error div zero"
+        except ZeroDivisionError:
+            return "error div zero"
 
-    def exp_func(number1:str):
+    def exp_func(self, number1:str):
         return str(float(number1) * float(number1))
 
-    def sqrt_func(number1:str):
+    def sqrt_func(self, number1:str):
         return str(sqrt(float(number1)))
 
 #    def get_pi():
-#        return pi   
-
-
+#        return pi
